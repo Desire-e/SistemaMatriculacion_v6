094ff1c1c105efa169e9fdb10d96371bb87947ca
Desire-e <desireeruedatorresalmeria@gmail.com>
2024-11-29 13:01:10 +0100

    Creada Clase EspecialidadProfesorado y test pasado;

---
+++ src/main/java/org/iesalandalus/programacion/matriculacion/dominio/EspecialidadProfesorado.java
@@ -0,0 +1,22 @@
+package org.iesalandalus.programacion.matriculacion.dominio;
+
+public enum EspecialidadProfesorado {
+    INFORMATICA("Informática"), SISTEMAS("Sistemas"), FOL("FOL");
+    private String cadenaAMostrar;
+
+    private EspecialidadProfesorado (String cadenaAMostrar) {
+        this.cadenaAMostrar = cadenaAMostrar;
+    }
+
+    public String imprimir (){
+        //  Método ordinal() da la posición del valor enumerado (empezando desde 0).
+        return ordinal() + ".-" + cadenaAMostrar;
+    }
+
+    @Override
+    public String toString() {
+        return "EspecialidadProfesorado{" +
+                "cadenaAMostrar='" + cadenaAMostrar + '\'' +
+                '}';
+    }
+}
