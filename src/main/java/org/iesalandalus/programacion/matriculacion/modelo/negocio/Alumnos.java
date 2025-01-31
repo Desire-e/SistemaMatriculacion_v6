05e7e56d3fea9832e3469c1b106f7946ab60369c
Desire-e <desireeruedatorresalmeria@gmail.com>
2025-02-01 00:57:52 +0100

    Modificada Clase Alumnos

--- src/main/java/org/iesalandalus/programacion/matriculacion/modelo/negocio/Alumnos.java
+++ src/main/java/org/iesalandalus/programacion/matriculacion/modelo/negocio/Alumnos.java
@@ -1,57 +1,56 @@
 package org.iesalandalus.programacion.matriculacion.modelo.negocio;
 
 import org.iesalandalus.programacion.matriculacion.modelo.dominio.Alumno;
+import org.iesalandalus.programacion.utilidades.Entrada;
 
 import javax.naming.OperationNotSupportedException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Alumnos {
-    private int capacidad;  // cantidad maxima que puede contener
-    private int tamano = 0;     // cantidad actual que contiene
-    private Alumno[] coleccionAlumnos;
+    private List<Alumno> coleccionAlumnos;
 
 
 
-    public Alumnos(int capacidad){
-
-        if (capacidad <= 0){
-            throw new IllegalArgumentException("ERROR: La capacidad debe ser mayor que cero.");
-        }
-
-        this.capacidad=capacidad;
-
-        /* Inicializada colección con la capacidad introducida por parámetros, de la que se hará más tarde una
-           copia profunda */
-        this.coleccionAlumnos = new Alumno[capacidad];
-
+    public Alumnos(/*int capacidad*/){
+        this.coleccionAlumnos = new ArrayList<>();
     }
 
-    public int getCapacidad() { return capacidad; }
-    public int getTamano() { return tamano; }
-    public Alumno[] get() { return copiaProfundaAlumnos(); }
+    /*public int getCapacidad() { return capacidad; }*/
+    /*public int getTamano() {
+        // size() devuelve el tamaño de la lista
+        return coleccionAlumnos.size();
+    }
+    */
+    public List<Alumno> get() { return copiaProfundaAlumnos(); }
 
 
 
-    // Copia profunda de Alumnos[] colecciónAlumnos
-    private Alumno [] copiaProfundaAlumnos(){
+    // Copia profunda de lista colecciónAlumnos
+    private List<Alumno> copiaProfundaAlumnos(){
+        /*
+        // Crear una lista copia de coleccionAlumnos (lista original)
+        List<Alumno> copiaProfunda = new ArrayList<>();
 
-        /* Crear un array copia de coleccionAlumnos (array original), del mismo tamaño */
-        Alumno[] copiaProfunda = new Alumno[coleccionAlumnos.length];
-
-        /* Recorrer el array coleccionAlumnos (original) */
-        for (int i = 0; i < coleccionAlumnos.length; i++) {
-
-            /* Verificar si la posición actual en array coleccionAlumnos (original) no es null */
-            if (coleccionAlumnos[i] != null) {
-                /* Crear una nueva posición en array copiaProfunda (copia): poner la posición actual que estamos
-                   recorriendo del array coleccionAlumnos (original) y se le asigna como valor a esa posición un
-                   objeto Alumno (usando el constructor copia, que tiene por parámetro Alumno alumno, y ese
-                   parámetro será = al objeto Alumno alumno que ya hay en la coleccionAlumnos en la posición actual
-                   que estamos recorriendo) */
-                copiaProfunda[i] = new Alumno(coleccionAlumnos[i]);
+        for (Alumno alumno : coleccionAlumnos) {
+            if (alumno != null) {
+                // Usamos el constructor copia de Alumno
+                // add() añade el elemento al conjunto. Si ya está, devuelve false y no lo añade.
+                copiaProfunda.add(new Alumno(alumno));
             }
         }
-
         return  copiaProfunda;
+         */
+
+        List<Alumno> copiaProfunda = coleccionAlumnos.stream()
+                .filter(alumno -> alumno != null)
+                .map(Alumno::new)   // Crea una copia de cada objeto Alumno usando el constructor copia.
+                                    // Usa el constructor copia de clase Alumno porque la lista de objetos obtenidos
+                                    // con coleccionAlumnos.stream().filter() son tipo Alumno y no nulos
+                .collect(Collectors.toList());  //Crea con esos objetos copiados una lista mutable
+                                                // (puedes borrar/añadir después objetos)
+        return copiaProfunda;
     }
 
 
@@ -62,76 +61,57 @@ public class Alumnos {
             throw new NullPointerException("ERROR: No se puede insertar un alumno nulo.");
         }
 
-        /* Buscar si existe ya el alumno a insertar */
-        int indice = buscarIndice(alumno);
-
-        /* Si buscarIndice devuelve índice que NO es -1 (devuelve una posición real del array),
-        es que el elemento YA EXISTE */
-        if (indice != -1){
+        if (coleccionAlumnos.contains(alumno)) {
             throw new OperationNotSupportedException("ERROR: Ya existe un alumno con ese dni.");
         }
 
-        if (capacidadSuperado(indice)) {
-            throw new OperationNotSupportedException("ERROR: No se aceptan más alumnos.");
-        }
-        else {
-            /* Insertar en la primera posición libre (la primera posción nula) */
-            for (int i = 0; i < coleccionAlumnos.length; i++) {
-                if (coleccionAlumnos[i] == null && !capacidadSuperado(i)) {
-
-                    coleccionAlumnos[i] = alumno;
-                    //Se crea una copia
-                    copiaProfundaAlumnos();
-                    //Para no crear copia del mismo alumno en todas las posiciones
-                    break;
-                }
-            }
-            tamano++;
-        }
+        coleccionAlumnos.add(alumno);
+        /*No es necesario llamar a copiaProfundaAlumnos(), cuando se invoque copiará todos los alumnos que
+        ya hay insertados*/
     }
 
 
 
+/*
     // Busca y devuelve el índice de un alummo, devuelve -1 si no existe
     private int buscarIndice(Alumno alumno) {
-
-        /* Variable que se devuelve si no se encuentra */
+        // Variable que se devuelve si no se encuentra
         int noExisteAlumno = -1;
-
-        /* Recorre array */
+        // Recorre array
         for (int i = 0; i < coleccionAlumnos.length; i++) {
-            /* Si en una posición, no nula, hay alumno = al alumno (el que buscamos) */
+            // Si en una posición, no nula, hay alumno = al alumno (el que buscamos)
             if (coleccionAlumnos[i] != null && coleccionAlumnos[i].equals(alumno)) {
-                /* SI ENCONTRÓ ALUMNO: Devuelve su indice */
+                // SI ENCONTRÓ ALUMNO: Devuelve su indice
                 return i;
             }
         }
-
-        /* Si al final no hay alumno = al alumno (el que buscamos) */
-        /* NO ENCONTRÓ ALUMNO: devuelve -1  */
+        // Si al final no hay alumno = al alumno (el que buscamos)
+        // NO ENCONTRÓ ALUMNO: devuelve -1
         return noExisteAlumno;
     }
+*/
 
 
+/*
     //APLICAR EN METODOS DONDE NO TIENEN COMO FIN MODIFICAR EL TAMAÑO ACTUAL DEL ARRAY
     private boolean tamanoSuperado(int indice){
-        /* Si supera tamaño actual del array, devuelve true */
+        // Si supera tamaño actual del array, devuelve true
         if (indice >= getTamano()) {
             return true;
         } else return false;
     }
+*/
 
 
-
+/*
     private boolean capacidadSuperado(int indice){
-        /* Si supera capacidad máxima del array, devuelve true */
+        // Si supera capacidad máxima del array, devuelve true
         if (tamano >= capacidad || indice>=capacidad) {
             return true;
         } else
             return false;
-
     }
-
+*/
 
 
     //Busca un alumno. Si existe devuelve el alumno, si no existe devuelve null
@@ -140,25 +120,18 @@ public class Alumnos {
             throw new NullPointerException("Alumno nulo no puede buscarse.");
         }
 
-        /* Buscar si existe ya el alumno a buscar */
-        int indice = buscarIndice(alumno);
-
-        /* Si buscarIndice devuelve índice que NO es -1 (devuelve una posición real del array),
-        es que el alumno SI EXISTE. Además si la posición no es nula */
-        if (indice != -1 && coleccionAlumnos[indice] != null){
-            for (Alumno alumnoCopia : copiaProfundaAlumnos()){
-                if (alumnoCopia.equals(alumno)){
-                    //Devuelve referencia copia
-                    return alumnoCopia;
-
-                }
-            }
-
-//            return coleccionAlumnos[indice];
+        // Si existe el alumno, devuelve copia de este
+        int indice;
+        if (coleccionAlumnos.contains(alumno)) {
+            // indexOf() devuelve el índice donde se encuentra el objeto indicado
+            indice=coleccionAlumnos.indexOf(alumno);
+            // get() devuelve el objeto exacto que se encuentra en el índice indicado
+            // alumno ahora tiene exactamente el valor del objeto que se encuentra en la lista
+            alumno = coleccionAlumnos.get(indice);
+            return new Alumno(alumno);
         }
-
-        /* Si indice = -1, NO EXISTE el alumno. Devuelve null */
-        return null;
+        // Si no existe, devuelve nulo
+        else return null;
     }
 
 
@@ -168,34 +141,29 @@ public class Alumnos {
             throw new NullPointerException("ERROR: No se puede borrar un alumno nulo.");
         }
 
-        int indice = buscarIndice(alumno);
-        if(indice == -1){
+        //Comprobar que existe
+        if (!coleccionAlumnos.contains(alumno)) {
             throw new OperationNotSupportedException("ERROR: No existe ningún alumno como el indicado.");
-        } else{
-            coleccionAlumnos[indice] = null;
-            desplazarUnaPosicionHaciaIzquierda(indice);
-            tamano--;
-
         }
+        //Si existe, lo borra
+        else coleccionAlumnos.remove(alumno);
     }
 
 
-
+/*
     private void desplazarUnaPosicionHaciaIzquierda(int indice){
-
         int i;
-        /* El índice debe de ir alumno por alumno (.length-1 porque si el array es coleccionAlumnos[5],
-        existen hasta alumno[4] como máximo) */
+        // El índice debe de ir alumno por alumno (.length-1 porque si el array es coleccionAlumnos[5],
+        // existen hasta alumno[4] como máximo)
         for ( i = indice; i < coleccionAlumnos.length - 1; i++){
 
-            /* La info del alumno en posición superior [i+1] pasa a esa posición [i] */
+            // La info del alumno en posición superior [i+1] pasa a esa posición [i]
             coleccionAlumnos[i]=coleccionAlumnos[i+1];
         }
-
-        /* La última posición alcanzada ahora no tiene alumno, pues se desplazó. Se asigna nulo */
+        // La última posición alcanzada ahora no tiene alumno, pues se desplazó. Se asigna nulo
         coleccionAlumnos[i]=null;
     }
-
+*/
 
 
 
