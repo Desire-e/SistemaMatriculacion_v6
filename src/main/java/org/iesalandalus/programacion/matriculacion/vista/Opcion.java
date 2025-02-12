1f8e7586e473baf8dc329f733a35fb776a2cb2ad
Desire-e <desireeruedatorresalmeria@gmail.com>
2025-02-12 14:37:39 +0100

    Cambiado enum Opcion

--- src/main/java/org/iesalandalus/programacion/matriculacion/vista/Opcion.java
+++ src/main/java/org/iesalandalus/programacion/matriculacion/vista/Opcion.java
@@ -2,26 +2,147 @@ package org.iesalandalus.programacion.matriculacion.vista;
 
 public enum Opcion {
 
-    SALIR("Salir"),
-    INSERTAR_ALUMNO("Insertar alumno"), BUSCAR_ALUMNO("Buscar alumno"),
-    BORRAR_ALUMNO("Borrar alumno"), MOSTRAR_ALUMNOS("Mostrar alumnos"),
 
-    INSERTAR_CICLO_FORMATIVO("Insertar ciclo formativo"),
-    BUSCAR_CICLO_FORMATIVO("Buscar ciclo formativo"),
-    BORRAR_CICLO_FORMATIVO("Borrar ciclo formativo"),
-    MOSTRAR_CICLOS_FORMATIVOS("Mostrar ciclos formativos"),
+    SALIR("Salir")
+    //CAMBIOS V.3:
+    {
+        @Override
+        // Aquí las instancias (salir, etc) heredan todas este método de este enum, con una implementación distinta
+        // en cada una. Por ejemplo, esta instancia usa este método para llamar a  terminar(), o sea, su correspondiente
+        // de la clase Vista.
+        public void ejecutar() {
+            vista.terminar();
+        }
+    },
 
-    INSERTAR_ASIGNATURA("Insertar asignatura"), BUSCAR_ASIGNATURA("Buscar asignatura"),
-    BORRAR_ASIGNATURA("Borrar asignatura"), MOSTRAR_ASIGNATURAS("Mostrar asignaturas"),
+    INSERTAR_ALUMNO("Insertar alumno"){
+        @Override
+        public void ejecutar(){
+            vista.insertarAlumno();
+        }
+    },
+    BUSCAR_ALUMNO("Buscar alumno"){
+        @Override
+        public void ejecutar(){
+            vista.buscarAlumno();
+        }
+    },
+    BORRAR_ALUMNO("Borrar alumno"){
+        @Override
+        public void ejecutar(){
+            vista.borrarAlumno();
+        }
+    },
+    MOSTRAR_ALUMNOS("Mostrar alumnos"){
+        @Override
+        public void ejecutar(){
+            vista.mostrarAlumnos();
+        }
+    },
 
-    INSERTAR_MATRICULA("Insertar matrícula"), BUSCAR_MATRICULA("Buscar matrícula"),
-    ANULAR_MATRICULA("Anular matrícula"), MOSTRAR_MATRICULAS("Mostrar matrículas"),
+    INSERTAR_CICLO_FORMATIVO("Insertar ciclo formativo"){
+        @Override
+        public void ejecutar(){
+            vista.insertarCicloFormativo();
+        }
+    },
+    BUSCAR_CICLO_FORMATIVO("Buscar ciclo formativo"){
+        @Override
+        public void ejecutar(){
+            vista.buscarCicloFormativo();
+        }
+    },
+    BORRAR_CICLO_FORMATIVO("Borrar ciclo formativo"){
+        @Override
+        public void ejecutar(){
+            vista.borrarCicloFormativo();
+        }
+    },
+    MOSTRAR_CICLOS_FORMATIVOS("Mostrar ciclos formativos"){
+        @Override
+        public void ejecutar(){
+            vista.mostrarCiclosFormativos();
+        }
+    },
 
-    MOSTRAR_MATRICULAS_ALUMNO("Mostrar matrículas alumno"),
-    MOSTRAR_MATRICULAS_CICLO_FORMATIVO("Mostrar matrículas ciclo formativo"),
-    MOSTRAR_MATRICULAS_CURSO_ACADEMICO("Mostrar matrículas curso académico");
+    INSERTAR_ASIGNATURA("Insertar asignatura"){
+        @Override
+        public void ejecutar(){
+            vista.insertarAsignatura();
+        }
+    },
 
-    private String cadenaAMostrar;
+    BUSCAR_ASIGNATURA("Buscar asignatura"){
+        @Override
+        public void ejecutar(){
+            vista.buscarAsignatura();
+        }
+    },
+
+    BORRAR_ASIGNATURA("Borrar asignatura"){
+        @Override
+        public void ejecutar(){
+            vista.borrarAsignatura();
+        }
+    },
+
+    MOSTRAR_ASIGNATURAS("Mostrar asignaturas"){
+        @Override
+        public void ejecutar(){
+            vista.mostrarAsignaturas();
+        }
+    },
+
+    INSERTAR_MATRICULA("Insertar matrícula"){
+        @Override
+        public void ejecutar(){
+            vista.insertarMatricula();
+        }
+    },
+
+    BUSCAR_MATRICULA("Buscar matrícula"){
+        @Override
+        public void ejecutar(){
+            vista.buscarMatricula();
+        }
+    },
+
+    ANULAR_MATRICULA("Anular matrícula"){
+        @Override
+        public void ejecutar(){
+            vista.anularMatricula();
+        }
+    },
+
+    MOSTRAR_MATRICULAS("Mostrar matrículas"){
+        @Override
+        public void ejecutar(){
+            vista.mostrarMatriculas();
+        }
+    },
+
+
+    MOSTRAR_MATRICULAS_ALUMNO("Mostrar matrículas alumno"){
+        @Override
+        public void ejecutar(){
+            vista.mostrarMatriculasPorAlumno();
+        }
+    },
+    MOSTRAR_MATRICULAS_CICLO_FORMATIVO("Mostrar matrículas ciclo formativo"){
+        @Override
+        public void ejecutar(){
+            vista.mostrarMatriculasPorCicloFormativo();
+        }
+    },
+    MOSTRAR_MATRICULAS_CURSO_ACADEMICO("Mostrar matrículas curso académico"){
+        @Override
+        public void ejecutar(){
+            vista.mostrarMatriculasPorCursoAcademico();
+        }
+    }
+    ;
+
+    private final String cadenaAMostrar;
 
     private Opcion (String cadenaAMostrar) {
         this.cadenaAMostrar = cadenaAMostrar;
@@ -31,5 +152,25 @@ public enum Opcion {
         //  Método ordinal() da la posición del valor enumerado (empezando desde 0).
         return ordinal() + ".-" + cadenaAMostrar;
     }
+
+
+    // CAMBIOS V.3:
+    private static Vista vista;
+
+    public static void setVista(Vista vista){
+        if (vista == null) {
+            throw new NullPointerException("La vista no puede ser nula.");
+        }
+        // Opcion.vista para llamar a la variable porque es static
+        Opcion.vista = vista;
+    }
+
+    // Método abstracto: método que obligatoriamente deben tener las clases herederas, pero no especifica desde el padre
+    // la implementación, pues varía en cada hija.
+
+    /* el método ejecutar deberá ser implementado en cada instancia del enum Opcion, llamando al método que le corresponda
+    de la clase Vista. Con esto se pretende que cada opción no solo contenga el mensaje que debe mostrarse por pantalla,
+    sino también, el método de la clase Vista que debe ser ejecutado cuando el usuario de la aplicación elija dicha opción. */
+    public abstract void ejecutar();
 
 }
